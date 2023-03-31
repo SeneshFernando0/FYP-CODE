30d661c6d227d3e9582b27a065f584c22eae69d7
Senesh Charinda Fernando <senesh.2018393@iit.ac.lk>
2023-03-31 17:33:05 +0530

    code file

---
+++ code/Dataset sentiment analysis_prep.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import urllib.request
+import numpy as np
+import json
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+data = pd.read_csv('Output_Dataset.csv',delimiter=",")
+
+row, col = data.shape
+
+print("rows : ",row)
+
+analyzer = SentimentIntensityAnalyzer()
+
+#creating data frame and the columns
+output_file = pd.DataFrame(columns=["ISBN","Book_Title","Description","sentiment_score_neg","sentiment_score_neu",
+                                    "sentiment_score_pos","sentiment_score_compound"])
+
+sample=""
+
+
+for i in range(2,row):
+
+    temp = analyzer.polarity_scores(data["Description"][i])
+    sample = str(temp)
+
+    sample = sample.replace(":", "").replace("{", "").replace("}", "").replace("'neg'", "").replace("'neu'", "") \
+        .replace("'pos'", "").replace("'compound'", "")
+
+    sample = sample.split(',')
+
+    output_file.loc[i] = [data["ISBN"][i],data["Book_Title"][i],data["Description"][i],sample[0],sample[1],sample[2],sample[3]]
+    print(data["Description"][i],"  ",sample[0],sample[1],sample[2],sample[3])
+
+
+
+output_file.to_csv('Finalset.csv', encoding='utf-8')
---
+++ code/ML model prototype.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from sklearn.metrics.pairwise import  cosine_similarity
+import numpy as np
+
+
+dataset = pd.read_csv('Finalset.csv')
+
+
+
+
+
+
+
+
+
+
+
+
